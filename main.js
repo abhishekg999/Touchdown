db55aa27582b42b511071faf3582bdec25d62c11
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2022-09-13 19:49:46 -0700

    DEBUG WORKING

--- main.js
+++ main.js
@@ -5,13 +5,21 @@ let StartPlayer;
 let EndPlayer;
 let PrevPlayer;
 
-let Guesses = [];
 let LatestGuessField = null;
 
 let MAXGUESSES = 6;
 let popular_players = [10586, 3011, 10081, 5882, 11075, 2503, 4044, 8357, 71, 10244, 8611, 6194, 7480, 11222, 8019, 2769, 5578, 10447, 6229, 5543, 9906, 7963, 5983, 2484, 2484, 1900, 3082, 6360, 11048, 341, 6186, 330, 7850, 696, 6022, 55, 9531, 7143, 10230, 2458, 6876, 6311, 2449, 9725, 3952, 116, 9318, 2493, 3, 6859, 1662, 7780, 4605, 1070, 8249, 4446, 6949, 7701, 9902, 8027, 4908, 8132, 4441, 3274, 5176, 4359, 2925, 7592, 1513, 11028, 11459, 2226, 7833, 3381, 5023, 10364, 1516, 3054, 11041, 5443, 1844, 6889, 2327, 10116, 2327, 6889, 309, 4850, 6534, 3936, 7948, 3314, 3181, 9629, 6126, 1072, 4231, 3934, 7897, 10726, 3738, 8442, 11446, 7633, 6768, 2523, 11, 4072, 8144, 9311, 25, 3740, 698, 9449, 6572, 6501, 7739, 9711, 2242, 2726, 2045, 8337, 4581, 7109, 2417, 2930, 11297, 6104, 8869, 7373, 9244, 1791, 8893, 1381, 8965, 752, 10032, 6267, 306, 4793, 2741, 2133, 7824, 581, 10908, 11142, 2406, 4096, 1971, 10878, 5450, 8715, 8989, 1224, 10616, 5672, 3086, 6914, 8913, 10951, 10182, 3671, 6833, 7019, 9221, 421, 2189, 1522, 2910, 10729, 6186];
 
-let Statistics = {"gamesPlayed": 24, "gamesWon": 19, "gameStats": {"1": 3, "2": 6, "3": 5, "4": 2, "5": 0, "6": 1, "fail": 2}, "currentStreak": 15, "maxStreak": 15};
+
+let Statistics = {"gamesPlayed": 0, "gamesWon": 0, "gameStats": {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "fail": 0}, "currentStreak": 0, "maxStreak": 0};
+let CurrentGame = {"id" : getDate(), "guesses": [], "finished": false, "won": false}
+
+let DONOTTOUCHTHIS = "";
+
+function getDate() {
+    return (Math.random() + 1).toString(36).substring(7); //ONLY USE FOR TESTING
+    //return new Date().toLocaleDateString();
+}
 
 function cyrb128(str) {
     let h1 = 1779033703, h2 = 3144134277,
@@ -45,7 +53,9 @@ function sfc32(a, b, c, d) {
 }
 
 function getPsrand(){
-    let today = new Date().toLocaleDateString();
+    let today;
+    today = getDate();
+    
     seed = cyrb128(today); 
     return sfc32(seed[0], seed[1], seed[2], seed[3]);
 }
@@ -66,26 +76,23 @@ function setTodayPlayers(){
     }
     console.log("End Player: " + EndPlayer);
 
+    PrevPlayer = StartPlayer;
 }
 
 async function loadPlayerId() {
     const response = await fetch("./player_lookup_table.json");
-    const playerIds = await response.json();
-
-    PlayerIds = playerIds;
+    PlayerIds = await response.json();
     console.log("loaded player ids")
 }
 
 async function loadPlayerTeammates() {
     const response = await fetch("./player_teammates.json");
-    const playerTeammates = await response.json();
-
-    PlayerTeammates = playerTeammates;
+    PlayerTeammates = await response.json();
     console.log("loaded player teammates")
 }
 
 
-function addGuess(id, acc, num) {
+function renderGuess(id, acc, num){
     let inner_div = document.createElement("div");
     inner_div.id = "guess-" + num;
     inner_div.classList.add('cell')
@@ -98,12 +105,23 @@ function addGuess(id, acc, num) {
 
     inner_div.innerHTML = PlayerIds[id]
 
-    document.getElementById('guess-pt').appendChild(inner_div)
+    document.getElementById('guess-pt').appendChild(inner_div)    
+}
+
+function addGuess(id, acc, num) {
+    if (acc == true) {
+        CurrentGame["guesses"].push([id, true]);
+        renderGuess(id, acc, num);
+    } else {
+        CurrentGame["guesses"].push([id, false]);
+        renderGuess(id, acc, num);
+    }
+    syncLocalStorage();
 }
 
 
 function getRemainingGuesses(){
-    return MAXGUESSES - Guesses.length;
+    return MAXGUESSES - CurrentGame["guesses"].length;
 }
 function updateGuessCnt(){
     document.getElementById("remaining-guess-cnt").innerHTML = getRemainingGuesses();
@@ -117,13 +135,13 @@ function generate_share_text() {
     share_str = "Touchdown\n";
     share_str += PlayerIds[StartPlayer] + " → " + PlayerIds[EndPlayer] + "\n🏈";
     for (var i in Guesses){
-        if (Guesses[i][1] === false){
+        if (CurrentGame["guesses"][i][1] === false){
             share_str += '🟥';
         } else {
             share_str += '🟩';
         }
     }
-    for (i = 0; i < 6 - Guesses.length; i++){
+    for (i = 0; i < 6 - CurrentGame["guesses"].length; i++){
         share_str += '⬜';
     }
     share_str += "\nhttps://touchdown.life";
@@ -138,15 +156,32 @@ function share(){
 
 function update_stats_modal() {
     document.getElementById('games-played').innerHTML = Statistics['gamesPlayed'];
-    document.getElementById('win-percentage').innerHTML = Math.round(Statistics['gamesWon']/Statistics['gamesPlayed'] * 100) + "%";
+
+    if (Statistics['gamesPlayed'] === 0){
+        document.getElementById('win-percentage').innerHTML = "0%"
+    } else {
+        document.getElementById('win-percentage').innerHTML = Math.round(Statistics['gamesWon']/Statistics['gamesPlayed'] * 100) + "%";
+    }
     document.getElementById('current-streak').innerHTML = Statistics['currentStreak'];
     document.getElementById('max-streak').innerHTML = Statistics['maxStreak'];
 
     var maxval = Math.max(...Object.values(Statistics["gameStats"]));
-    for (var i = 1; i <= 6; i++){
-        document.getElementById('stats-r-' + i).innerHTML = Statistics["gameStats"][i];
-        var s_w_calc = (92 * Statistics["gameStats"][i]/maxval) + 8;
-        document.getElementById('stats-r-' + i).style.width = s_w_calc + "%";
+    if (maxval === 0){
+        for (var i = 1; i <= 6; i++){
+            document.getElementById('stats-r-' + i).innerHTML = Statistics["gameStats"][i];
+            var s_w_calc = 8;
+            document.getElementById('stats-r-' + i).style.width = s_w_calc + "%";
+        }
+    } else {
+        for (var i = 1; i <= 6; i++){
+            document.getElementById('stats-r-' + i).innerHTML = Statistics["gameStats"][i];
+            var s_w_calc = (92 * Statistics["gameStats"][i]/maxval) + 8;
+            document.getElementById('stats-r-' + i).style.width = s_w_calc + "%";
+        }
+    }
+
+    if (CurrentGame["finished"] && CurrentGame["won"]) {
+        document.getElementById('stats-r-' + CurrentGame["guesses"].length).classList.add("current");
     }
 }
 
@@ -204,12 +239,16 @@ function bfs(){
             explored.push(node);
         }
     }
-
     return null;
 }
 
+
+function syncLocalStorage(){
+    localStorage.setItem("CurrentGame", JSON.stringify(CurrentGame));
+    localStorage.setItem("Statistics", JSON.stringify(Statistics));
+}
+
 async function init() {
-    window.localStorage.clear();
     await loadPlayerId();
     await loadPlayerTeammates();
 
@@ -219,7 +258,14 @@ async function init() {
     }
 
     setTodayPlayers();
-    PrevPlayer = StartPlayer;
+    if (localStorage.getItem("CurrentGame") !== null && CurrentGame["id"] == getDate()){
+        CurrentGame = JSON.parse(localStorage.getItem("CurrentGame"));  
+    }
+
+    if (localStorage.getItem("Statistics") !== null){
+        Statistics = JSON.parse(localStorage.getItem("Statistics"));
+    }
+    syncLocalStorage();
 
     document.getElementById("p-start").innerHTML = PlayerIds[StartPlayer];
     document.getElementById("p-end").innerHTML = PlayerIds[EndPlayer];
@@ -228,18 +274,39 @@ async function init() {
     document.getElementById("start-player").innerHTML = PlayerIds[StartPlayer];
     document.getElementById("end-player").innerHTML = PlayerIds[EndPlayer];
 
-    for (let x = 0; x < Guesses.length; x++) {
-        addGuess(Guesses[x][0], Guesses[x][1], x)
+    for (let x = 0; x < CurrentGame["guesses"].length; x++) {
+        renderGuess(CurrentGame["guesses"][x][0], CurrentGame["guesses"][x][1], x)
     }
     updateGuessCnt();
+
+    if (CurrentGame["won"]){
+        document.getElementById("next-pt").style.setProperty("display", "none", "important");
+    }
 
     //initialized
     document.getElementById("loading-screen").style.display = "none";
     autocomplete(document.getElementById("ui"), player_names);
 }
 
-function game_end() {
+function game_end(won) {
+    CurrentGame["finished"] = true;
+    CurrentGame["won"] = won;
     document.getElementById("next-pt").style.setProperty("display", "none", "important");
+
+    Statistics["gamesPlayed"]++;
+    if (won){
+        Statistics["currentStreak"]++;
+        Statistics["gamesWon"]++;
+        if (Statistics["currentStreak"] > Statistics["maxStreak"]){
+            Statistics["maxStreak"] = Statistics["currentStreak"];
+        } 
+        Statistics["gameStats"][CurrentGame["guesses"].length]++;
+    } else {
+        Statistics["currentStreak"] = 0;
+        Statistics["gameStats"]["fail"]++;
+    }
+
+    syncLocalStorage();
 }
 
 function autocomplete(inp, arr) {
@@ -283,23 +350,21 @@ function autocomplete(inp, arr) {
 
                     // check if player is correct            
 	                if (PlayerTeammates[PrevPlayer].includes(input_id)) {
-	                    Guesses.push([input_id, true])
 	                    PrevPlayer = parseInt(input_id);
-	                    addGuess(input_id, true, Guesses.length - 1)
+	                    addGuess(input_id, true, CurrentGame["guesses"].length - 1)
     	                if (PlayerTeammates[PrevPlayer].includes(EndPlayer)) { //if player won remove next field box
-                            game_end();
+                            game_end(true);
     	                }
 
 	                } else {
-	                    Guesses.push([input_id, false])
-	                    addGuess(input_id, false, Guesses.length - 1)
+	                    addGuess(input_id, false, CurrentGame["guesses"].length - 1)
 	                }
 
                     updateGuessCnt();
 	                inp.innerHTML = "";
 
                     if (getRemainingGuesses() <= 0){
-                        game_end();
+                        game_end(false);
                     }
                 });
                 a.appendChild(b);
