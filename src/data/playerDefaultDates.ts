21340d9c2965d4c260b608ea16d063ccc886fc09
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2025-11-06 00:24:40 -0800

    fix: dates

--- src/data/playerDefaultDates.ts
+++ src/data/playerDefaultDates.ts
@@ -2,5 +2,5 @@ import type { PlayerDefaultDates } from "../types";
 
 export const playerDefaultDates: PlayerDefaultDates = {
     // New API Migration
-    "11/6/2025":[3387,2544]
+    "11/6/2025":[4478,3391]
 } as const;
