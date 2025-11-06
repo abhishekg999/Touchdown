a388e183b07dc8038309f17b8c3c15263541b4e4
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2025-11-06 00:15:48 -0800

    migration

--- src/data/playerDefaultDates.ts
+++ src/data/playerDefaultDates.ts
@@ -1,3 +1,6 @@
 import type { PlayerDefaultDates } from "../types";
 
-export const playerDefaultDates: PlayerDefaultDates = {"9/14/2023":[4940,6412],"2/11/2024":[1375,8901]} as const;
+export const playerDefaultDates: PlayerDefaultDates = {
+    // New API Migration
+    "11/6/2025":[3387,2544]
+} as const;
